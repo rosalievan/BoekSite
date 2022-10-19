d175b0685dac7420f38598d60525c9a8b2214359
rosalievan <r.vanonzenoort98@gmail.com>
2022-10-19 11:12:34 +0100

    Rendering data from backend

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -5,55 +5,60 @@ export class FetchData extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { forecasts: [], loading: true };
+    this.state = { books: [], loading: true };
   }
 
   componentDidMount() {
     this.populateWeatherData();
   }
 
-  static renderForecastsTable(forecasts) {
+  static renderBooksTable(books) {
+
     return (
       <table className='table table-striped' aria-labelledby="tabelLabel">
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
+            <th>Title</th>
+            <th>Read</th>
+            {/* <th>Temp. (F)</th>
+            <th>Summary</th> */}
           </tr>
         </thead>
         <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
+          {books.map(book =>
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{String(book.read)}</td>
+              {/* <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td> */}
             </tr>
           )}
         </tbody>
       </table>
+
     );
   }
 
   render() {
     let contents = this.state.loading
       ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+      : FetchData.renderBooksTable(this.state.books);
 
     return (
       <div>
         <h1 id="tabelLabel" >Weather forecast</h1>
         <p>This component demonstrates fetching data from the server.</p>
+        
         {contents}
       </div>
     );
   }
 
   async populateWeatherData() {
-    const response = await fetch('weatherforecast');
+    const response = await fetch('books');
     const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
+    this.setState({ books: data, loading: false });
+
+    
   }
 }
